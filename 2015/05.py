560f5e597c0f01ca212f9e9a1a649675e295ba8a
Wesley Lether <w.lether@lethermade.nl>
2024-12-01 21:17:21 +0100

    feat: add 2015 05

---
+++ 2015/05.py
@@ -0,0 +1,39 @@
+import re
+import time
+from collections import defaultdict, Counter, deque
+from copy import deepcopy
+from math import gcd
+
+from modules.advent_of_code import Timer, answer_part_one, answer_part_two, get_input
+
+timer = Timer()
+input_file = get_input()
+timer.start_timer()
+
+# Start coding here
+# ==========================================================================
+p1 = 0
+p2 = 0
+
+for string in input_file.split():
+    if re.search(r"(..).*\1", string) and re.search(r"(.).\1", string):
+        p2 += 1
+
+    vowels = re.findall(r"[aeiou]", string)
+    if len(vowels) < 3:
+        continue
+    if not re.search(r"(.)\1", string):
+        continue
+    if re.search(r"ab|cd|pq|xy", string):
+        continue
+    p1 += 1
+
+
+# Print the answers here
+# ==========================================================================
+answer_part_one(p1)
+answer_part_two(p2)
+
+# End of Code
+# ==========================================================================
+timer.end_timer()
