4885c0c737f467503757e572d5a53ea69ccffb76
Wesley Lether <w.lether@lethermade.nl>
2024-12-02 21:19:30 +0100

    feat: add 2015 07

---
+++ 2015/07.py
@@ -0,0 +1,59 @@
+import re
+import time
+from collections import defaultdict, Counter, deque
+from copy import deepcopy
+from math import gcd
+
+from modules.advent_of_code import Timer, answer_part_one, answer_part_two, get_input
+
+timer = Timer()
+input_file = get_input()
+timer.start_timer()
+
+# Start coding here
+# ==========================================================================
+instructions = {}
+results = {}
+
+for command in input_file.split("\n"):
+    signals, wire = command.split("->")
+    instructions[wire.strip()] = signals.strip().split(" ")
+
+
+def calculate(name):
+    try:
+        return int(name)
+    except ValueError:
+        pass
+
+    result = None
+    if name not in results:
+        signals = instructions[name]
+        if len(signals) == 1:
+            result = calculate(signals[0])
+        else:
+            operation = signals[-2]
+            if operation == "AND":
+                result = calculate(signals[0]) & calculate(signals[2])
+            elif operation == "OR":
+                result = calculate(signals[0]) | calculate(signals[2])
+            elif operation == "NOT":
+                result = ~calculate(signals[1]) & 0xFFFF
+            elif operation == "RSHIFT":
+                result = calculate(signals[0]) >> calculate(signals[2])
+            elif operation == "LSHIFT":
+                result = calculate(signals[0]) << calculate(signals[2])
+        results[name] = result
+    return results[name]
+
+
+# Print the answers here
+# ==========================================================================
+p1 = calculate("a")
+answer_part_one(p1)
+results = {"b": p1}
+answer_part_two(calculate("a"))
+
+# End of Code
+# ==========================================================================
+timer.end_timer()
