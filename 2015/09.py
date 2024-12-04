5572672702c3ef3326cc1c66c41f0053d60ae8dd
Wesley Lether <w.lether@lethermade.nl>
2024-12-04 21:24:01 +0100

    feat: add 2015 09 & 10

---
+++ 2015/09.py
@@ -0,0 +1,56 @@
+import re
+import time
+from collections import defaultdict, Counter, deque
+from copy import deepcopy
+from itertools import permutations
+from math import gcd
+
+from modules.advent_of_code import Timer, answer_part_one, answer_part_two, get_input, dd, ddd
+
+timer = Timer()
+input_file = get_input()
+timer.start_timer()
+
+# Start coding here
+# ==========================================================================
+directions = {}
+for direction in input_file.split("\n"):
+    match = re.match(r"(\w+) to (\w+) = (\d+)", direction)
+    if match:
+        start, end, distance = match.groups()
+        if start not in directions:
+            directions[start] = {}
+
+        if end not in directions:
+            directions[end] = {}
+
+        directions[start][end] = int(distance)
+        directions[end][start] = int(distance)
+
+cities = list(directions.keys())
+
+
+def calculate_route_distance(route):
+    total_distance = 0
+    for i in range(len(route) - 1):
+        total_distance += directions[route[i]][route[i + 1]]
+    return total_distance
+
+
+shortest_distance = float("inf")
+longest_distance = float("-inf")
+for route in permutations(cities):
+    current_distance = calculate_route_distance(route)
+    if current_distance < shortest_distance:
+        shortest_distance = current_distance
+    if current_distance > longest_distance:
+        longest_distance = current_distance
+
+# Print the answers here
+# ==========================================================================
+answer_part_one(shortest_distance)
+answer_part_two(longest_distance)
+
+# End of Code
+# ==========================================================================
+timer.end_timer()
---
+++ 2015/10.py
@@ -0,0 +1,40 @@
+import re
+import time
+from collections import defaultdict, Counter, deque
+from copy import deepcopy
+from math import gcd
+
+from modules.advent_of_code import Timer, answer_part_one, answer_part_two, get_input, ddd, dd
+
+timer = Timer()
+input_file = get_input()
+timer.start_timer()
+
+
+# Start coding here
+# ==========================================================================
+def look_and_say(sequence):
+    result = ""
+    i = 0
+    while i < len(sequence):
+        count = 1
+        while i + 1 < len(sequence) and sequence[i] == sequence[i + 1]:
+            i += 1
+            count += 1
+        result += str(count) + sequence[i]
+        i += 1
+    return result
+
+
+p1 = input_file
+for _ in range(40):
+    p1 = look_and_say(p1)
+answer_part_one(len(p1))
+timer.end_timer()
+
+timer.start_timer()
+p2 = input_file
+for _ in range(50):
+    p2 = look_and_say(p2)
+answer_part_one(len(p2))
+timer.end_timer()
