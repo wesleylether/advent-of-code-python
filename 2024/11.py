dc9e399a5a2fd025070d59636b15e7305b4a4587
Wesley Lether <w.lether@lethermade.nl>
2024-12-11 11:47:18 +0100

    feat: add 2024 10 + refactor grid

--- 2024/11.py
+++ 2024/11.py
@@ -12,7 +12,7 @@ values = {v: 1 for v in input_file.split()}
 index = 0
 while index < 75:
     index += 1
-    print(f"Index: {index}")
+    # print(f"Index: {index}")
 
     new_values = {}
 
@@ -36,6 +36,50 @@ while index < 75:
 
     values = new_values
     # print(values)
+    if index == 25:
+        answer_part_one(sum(values.values()))
+
+# Print the answers here
+# ==========================================================================
+answer_part_two(sum(values.values()))
+
+# End of Code
+# ==========================================================================
+timer.end_timer()
+
+
+# Lambda variant
+# ==========================================================================
+
+timer.start_timer()
+
+# Start coding here
+# ==========================================================================
+values = {v: 1 for v in input_file.split()}
+
+append_dict = lambda d, k, v: d.update({k: d.get(k, 0) + v})
+
+process_item = lambda item, count: (
+    [("1", count)]
+    if item == "0"
+    else (
+        [(str(int(item[: len(item) // 2])), count), (str(int(item[len(item) // 2 :])), count)]
+        if len(item) % 2 == 0
+        else [(str(int(item) * 2024), count)]
+    )
+)
+
+index = 0
+while index < 75:
+    index += 1
+    # print(f"Index: {index}")
+
+    new_values = {}
+    for item, count in values.items():
+        for k, v in process_item(item, count):
+            append_dict(new_values, k, v)
+
+    values = new_values
     if index == 25:
         answer_part_one(sum(values.values()))
 
