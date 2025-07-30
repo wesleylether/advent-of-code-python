9a3865d759e30106cbe875dff813c9381e22a3ee
Wesley Lether <w.lether@lethermade.nl>
2025-07-30 22:36:39 +0200

    feat: added 2015 20

---
+++ 2015/20.py
@@ -0,0 +1,49 @@
+import re
+import time
+from collections import defaultdict, Counter, deque
+from copy import deepcopy
+from math import gcd
+
+from modules.advent_of_code import solve
+from modules.grid import Grid
+from modules.helpers import dd, ddd, pp
+from modules.list import List
+
+# Start coding here
+# ==========================================================================
+def part_one(data):
+    target = int(data)
+    houses = [0] * (target // 10)
+
+    for elf in range(1, len(houses)):
+        for house in range(elf, len(houses), elf):
+            houses[house] += elf * 10
+
+    for house, presents in enumerate(houses):
+        if presents >= target:
+            return house, presents
+    return None
+
+
+def part_two(data):
+    target = int(data)
+    houses = [0] * (target // 11)
+
+    for elf in range(1, len(houses)):
+        visit_count = 0
+        for house in range(elf, len(houses), elf):
+            if visit_count >= 50:
+                break
+            houses[house] += elf * 11
+            visit_count += 1
+
+    for house, presents in enumerate(houses):
+        if presents >= target:
+            return house, presents
+    return None
+
+
+# Answers
+# ==========================================================================
+# solve(part_one)
+solve(part_two)
